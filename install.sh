35dcf08cdc5b356c406d136d9cb3ddd20da9fa85
Eric Bortz <e.bortz@netstack.de>
2023-03-21 13:54:34 +0100

    now automatically downloads and installs the tar ball

--- install.sh
+++ install.sh
@@ -1,52 +1,43 @@
 #!/bin/bash
 
 PNG_URL="https://txt.osk.sh/branding/tetrio-color.png"
+TAR_URL="https://tetr.io/about/desktop/builds/TETR.IO%20Setup.tar.gz"
 DESKTOP_ENTRY_PATH="$HOME/.local/share/applications/"
-BIN_PATH="$HOME/.local/"
+LOCAL_PATH="$HOME/.local/"
 
-help() {
-    echo Please use: ./install.sh /path/to/TETR.IO\\ Setup.tar.gz
-}
+# download tar ball
+wget "$TAR_URL"
 
-if [ $# == 0 ]; then
-    help
-    exit 1
-else
-    TETRIO_PATH="$1"
-fi
+# extract files
+tar -vxf "TETR.IO Setup.tar.gz"
 
-# test to see if path is valid
-
-if ! [ -e "$TETRIO_PATH" ]; then
-    echo "File not found."
-    exit 1
-fi
-
-# extract files and grab the extracted directory name
-tar -vxf "$TETRIO_PATH"
+# grab the extracted directory name
 dir_name=$(find . -name "tetrio-desktop*" -type d)
-dir_name=${dir_name:2}
+dir_name=${dir_name:2} # cut off the ./ from the start
+
 # extract version number of tetrio
 # 15 is for the strings of "tetrio-desktop"
-# -1 cuts of the "/" from the directory
 VERSION=${dir_name:15}
-### move files to destination
-mv "$dir_name" "$BIN_PATH"
-EXEC_PATH="$BIN_PATH$dir_name/"
 
-###### Desktop integration
+# move files to destination
+mv "$dir_name" "$LOCAL_PATH"
+FILE_LOCATION="$LOCAL_PATH$dir_name/"
+
+### Desktop integration ###
 
 # download the tetrio logo to the tetrio path
-ICON_PATH="$EXEC_PATH""tetrio-color.png"
-curl --create-dirs --output $ICON_PATH $PNG_URL
+ICON_PATH="$FILE_LOCATION""tetrio-color.png"
+wget $PNG_URL
+mv "tetrio-color.png" $ICON_PATH
 
+# Copy the desktop file
 cp ./tetrio.desktop $DESKTOP_ENTRY_PATH
 ENTRY="$DESKTOP_ENTRY_PATH""tetrio.desktop"
-EXEC_PATH="$EXEC_PATH""tetrio-desktop"
+EXECUTABLE_LOCATION="$FILE_LOCATION""tetrio-desktop"
 
 # set attributes
 sed -i "s+VERSION+$VERSION+g" $ENTRY
-sed -i "s+EXEC_PATH+$EXEC_PATH+g" $ENTRY
+sed -i "s+EXEC_PATH+$EXECUTABLE_LOCATION+g" $ENTRY
 sed -i "s+ICON_PATH+$ICON_PATH+g" $ENTRY
 
 # update desktop entries
