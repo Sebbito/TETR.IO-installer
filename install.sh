c4e2b76fe71abcf7c056c9cd74b03e8e1678f39f
Sebito <code-sebito@mailbox.org>
2025-02-09 13:24:45 +0100

    Refactor/Update/Fix Script
    This commit changes/fixes the following things:
    
    - Download newest TETR.IO version*
    - Move desktop file template into install script and replacement inline
    - Remove wget from dependencies in favor of curl
    - Added some error checks for dependencies and directories that need to
      be present
    - Icons are placed into correct directory
    - Fix desktop file `Version` value
    
    *Sadly osk decided to not stick with an URL that points to the latest
    TETR.IO version. This means that the version now has to be hard coded
    unless i want to scape the HTML and extract the version that way. I
    don't think it's worth the effort so I'll just have to live with the
    fact that this script has to be updated every now and then. Maybe in the
    future I'm annoyed enough to fix it.

--- install.sh
+++ install.sh
@@ -1,49 +1,74 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# This script downloads and installs TETR.IO to your ~/.local/ directory and
+# places a desktop integration file in ~/.local/share/applications/.
+# Sadly the old 'latest' version path doesn't work anymore so versions will be
+# hardcoded. I don't want to bother with webscraping for now.
+
+# These variables may break any time since they rely on osk
 
 PNG_URL="https://txt.osk.sh/branding/tetrio-color.png"
-TAR_URL="https://tetr.io/about/desktop/builds/TETR.IO%20Setup.tar.gz"
-DESKTOP_FILE_URL="https://raw.githubusercontent.com/Sebbito/TETR.IO-installer/main/tetrio.desktop"
+TAR_NAME="TETR.IO%20Setup.tar.gz"
+VERSION="9.0.0"
+SHORT_VERSION="9"
+TAR_URL="https://tetr.io/about/desktop/builds/$SHORT_VERSION/$TAR_NAME"
+
+# Fixed paths and variables
+
 DESKTOP_ENTRY_PATH="$HOME/.local/share/applications/"
+ICON_PATH="$HOME/.local/share/icons/hicolor/213x213"
 LOCAL_PATH="$HOME/.local/"
+DIRNAME="tetrio-desktop-$VERSION"
+TETRIO_INSTALL_LOCATION="$LOCAL_PATH$DIRNAME"
 
-# download tar ball
-wget "$TAR_URL"
+if [ ! $(which update-desktop-database) ]; then
+  echo "update-desktop-database not installed. Aborting."
+  echo "Maybe you don't have desktop support?"
+  exit 1
+fi
 
-# extract files
-tar -vxf "TETR.IO Setup.tar.gz"
+for cmd in "curl" "tar"; do
+  if [ ! $(which $cmd) ]; then
+    echo "$cmd not installed. Aborting."
+    exit 1
+  fi
+  echo "$cmd installed"
+done
 
-# grab the extracted directory name
-dir_name=$(find . -name "tetrio-desktop*" -type d)
-dir_name=${dir_name:2} # cut off the ./ from the start
+if [ ! -d $DESKTOP_ENTRY_PATH ]; then
+  echo "Icon path $DESKTOP_ENTRY_PATH not found"
+  echo "Creating directory $DESKTOP_ENTRY_PATH"
+  mkdir -p $DESKTOP_ENTRY_PATH
+fi
 
-# extract version number of tetrio
-# 15 is for the strings of "tetrio-desktop"
-VERSION=${dir_name:15}
+if [ ! -d $ICON_PATH ]; then
+  echo "Dektop entry path $ICON_PATH not found"
+  echo "Creating directory $ICON_PATH"
+  mkdir -p $ICON_PATH
+fi
 
-# move files to destination
-mv "$dir_name" "$LOCAL_PATH"
-FILE_LOCATION="$LOCAL_PATH$dir_name/"
+### TETR.IO App ###
+
+echo "Downloading version $VERSION of TETR.IO"
+curl -O "$TAR_URL" >/dev/null
+tar -vxf "$TAR_NAME"
+mv "$DIRNAME" "$LOCAL_PATH"
 
 ### Desktop integration ###
 
-# download the tetrio logo to the tetrio path
-ICON_PATH="$FILE_LOCATION""tetrio-color.png"
-wget $PNG_URL
+curl -O $PNG_URL >/dev/null
 mv "tetrio-color.png" $ICON_PATH
 
-# Copy the desktop file
-wget "$DESKTOP_FILE_URL"
-mv ./tetrio.desktop $DESKTOP_ENTRY_PATH
-ENTRY="$DESKTOP_ENTRY_PATH""tetrio.desktop"
-EXECUTABLE_LOCATION="$FILE_LOCATION""tetrio-desktop"
+echo "[Desktop Entry]
+Version=1.0
+Type=Application
+Name=TETR.IO
+GenericName=Stacker
+Comment=TETR.IO is a free-to-win modern yet familiar online stacker.
+TryExec=$TETRIO_INSTALL_LOCATION/TETR.IO
+Exec=$TETRIO_INSTALL_LOCATION/TETR.IO
+Icon=$ICON_PATH/tetrio-color.png
+Categories=Game;" >"$DESKTOP_ENTRY_PATH/tetrio.desktop"
 
-# set attributes
-sed -i "s+VERSION+$VERSION+g" $ENTRY
-sed -i "s+EXEC_PATH+$EXECUTABLE_LOCATION+g" $ENTRY
-sed -i "s+ICON_PATH+$ICON_PATH+g" $ENTRY
-
-# update desktop entries
 update-desktop-database ~/.local/share/applications
-
-# remove the tarball
-rm "TETR.IO Setup.tar.gz"
+rm "$TAR_NAME"
